cec8699d064d4b3e65bf3f8d74d461656921b2ed
Loïc Le Malliaud <loic@evenium.com>
2019-10-11 10:10:01 +0200

    fix: re-use FileProvider

--- src/android/CameraLauncher.java
+++ src/android/CameraLauncher.java
@@ -302,10 +302,9 @@ public class CameraLauncher extends CordovaPlugin implements MediaScannerConnect
 
         // Specify file so that large image is captured and returned
         File photo = createCaptureFile(encodingType);
-        this.imageUri = new CordovaUri(Uri.parse(photo.getAbsolutePath()));
-        /*this.imageUri = new CordovaUri(FileProvider.getUriForFile(cordova.getActivity(),
+        this.imageUri = new CordovaUri(FileProvider.getUriForFile(cordova.getActivity(),
                 applicationId + ".provider",
-                photo));*/
+                photo));
         intent.putExtra(MediaStore.EXTRA_OUTPUT, imageUri.getCorrectUri());
         //We can write to this URI, this will hopefully allow us to write files to get to the next step
         intent.addFlags(Intent.FLAG_GRANT_WRITE_URI_PERMISSION);
